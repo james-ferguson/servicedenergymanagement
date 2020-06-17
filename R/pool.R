e0442cfdaac4b0036e4b5c899eae39f6fa17a8f4
james-ferguson <james@kwiqly.com>
2020-06-17 12:58:05 +0200

    explicit pool reference

--- R/pool.R
+++ R/pool.R
@@ -3,6 +3,6 @@
 get_pool <- function(){
   options(scipen = 12)
   conn_args <- config::get("dataconnection")
-  dbPool(odbc::odbc(), Driver = conn_args$driver, Server = conn_args$server, UID = conn_args$uid, PWD = conn_args$pwd,
+  pool::dbPool(odbc::odbc(), Driver = conn_args$driver, Server = conn_args$server, UID = conn_args$uid, PWD = conn_args$pwd,
                        Port = conn_args$port, Database = conn_args$database, BoolsAsChar = 0, MaxVarcharSize = 0)
 }
